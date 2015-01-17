3fd7449b4402b9718651ad92024e8df145be60a1
TorqyTheWolf <haloforge117@gmail.com>
2015-01-17 12:21:02 -0400

    Updated files

--- src/org/ilite/ui/ApplicationMain.java
+++ src/org/ilite/ui/ApplicationMain.java
@@ -1,5 +1,14 @@
 package org.ilite.ui;
+import org.ilite.ui.y2015.UIBuilder;
 
-public class Application {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class ApplicationMain extends Application
+{
+	public void start(Stage stage)
+	{
+		UIBuilder.generateUI(stage);
+	}
+	
 }
