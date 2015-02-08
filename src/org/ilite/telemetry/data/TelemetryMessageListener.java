5bb810cc9c1ba13e601e89e11d82598a659c78f9
KellyMichael <rockymkelly@gmail.com>
2015-02-08 14:37:39 -0500

    Hey there charlie brown

--- src/org/ilite/telemetry/data/TelemetryMessageListener.java
+++ src/org/ilite/telemetry/data/TelemetryMessageListener.java
@@ -1,5 +1,6 @@
 package org.ilite.telemetry.data;
 
+import org.ilite.ui.table.TableManager;
 import org.ilite.ui.y2015.SideBar;
 import org.usfirst.frc.team1885.robot.comms.TelemetryMessage;
 
@@ -8,5 +9,6 @@ public interface TelemetryMessageListener
 	public static void receivedMessage(TelemetryMessage msg)
 	{
 		SideBar.update(msg);
+		TableManager.getUpdate(msg);
 	}
 }
--- src/org/ilite/ui/table/Table.java
+++ src/org/ilite/ui/table/Table.java
@@ -1,5 +1,8 @@
 package org.ilite.ui.table;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
@@ -10,33 +13,50 @@ import javafx.stage.Stage;
 import org.ilite.telemetry.data.y2015.EData2015;
 
 public class Table {
+	
+	private static GridPane pane;
+	
+	private static List<Double> values = new ArrayList<Double>();
 
 	public static void buildSimpleTable(Stage stage){
-		GridPane grid = new GridPane();
-		grid.setHgap(20);
-		int counter = 0;
-		for(EData2015 data : EData2015.values()){
-			Text textNamePort = new Text("Port " + data.getPortNumber() + " " + (data.getDisplayLabel() != null ? data.getDisplayLabel() : data.getTelemetryType().name()));
-			Text textValue = new Text("VALUE");
-			Text textAverage = new Text("AVERAGE");
-			grid.add(textNamePort, 0, counter);
-			grid.add(textValue, 1, counter);
-			grid.add(textAverage, 2, counter);
-			counter ++;
+		for(int i = 0; i < values.size(); i++){
+			values.set(i, 0.0);
 		}
+		pane = new GridPane();
+		pane.setHgap(20);
+		
+		refreshTable();
 		
 		Scene scene = new Scene(new Group());
 		scene.getStylesheets().addAll("/org/ilite/uitools/css/main.css", "/org/ilite/ui/css/UIManager.css");
 		stage.setTitle("RECYCLE RUSH 2015 UI");
 		stage.setWidth(800);
 		stage.setHeight(800);
-		((Group) scene.getRoot()).getChildren().add(grid);
+		((Group) scene.getRoot()).getChildren().add(pane);
 
 		stage.setScene(scene);
 		stage.show();
 		stage.setFullScreen(true);
 		scene.setFill(Color.TRANSPARENT);
 		
+		
+		
+	}
+	
+	public static void setValues(List<Double> vals){
+		values = vals;
+	}
+	
+	public static void refreshTable(){
+		pane.setHgap(20);
+		int counter = 0;
+		for(EData2015 data : EData2015.values()){
+			Text textNamePort = new Text("Port " + data.getPortNumber() + " " + (data.getDisplayLabel() != null ? data.getDisplayLabel() : data.getTelemetryType().name()));
+			Text textValue = new Text(values.get(counter).doubleValue() + "");
+			pane.add(textNamePort, 0, counter);
+			pane.add(textValue, 1, counter);
+			counter++;
+		}
 		
 	}
 }
