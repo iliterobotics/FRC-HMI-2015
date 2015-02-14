638ce07235e7b4971f9550d5f28e83e7ee276af7
nrleuenb <nrleuenberger@gmail.com>
2015-02-14 12:04:01 -0500

    files

--- src/org/ilite/ui/y2015/MainView.java
+++ src/org/ilite/ui/y2015/MainView.java
@@ -1,6 +1,15 @@
 package org.ilite.ui.y2015;
 
+import javafx.embed.swing.SwingNode;
+import javafx.scene.layout.VBox;
+
 public class MainView 
 {
-
+	private static SwingNode node;
+	
+	public static void generateMainView()
+	{
+		node.prefHeight(480);
+		node.prefWidth(height)
+	}
 }
--- src/org/ilite/ui/y2015/WidgetBar.java
+++ src/org/ilite/ui/y2015/WidgetBar.java
@@ -1,6 +1,14 @@
 package org.ilite.ui.y2015;
 
+import javafx.scene.layout.VBox;
+
 public class WidgetBar 
 {
-
+	private static VBox widgetBar;
+	
+	public static VBox generateWidgets()
+	{
+		widgetBar = new VBox(15);
+		return null;
+	}
 }
